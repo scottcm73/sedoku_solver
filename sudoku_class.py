7f770f0a7be6a24c0c0bd1c3c9f5f7e45df2faf4
scottcm73 <scottcmcmahan@gmail.com>
2020-10-20 23:59:54 -0500

    Added function for making 3 by 3 blocks

--- sudoku_class.py
+++ sudoku_class.py
@@ -11,15 +11,26 @@ class Sudoku:
 
         # Replace zeros with np.nan because zeros act as placeholders.
         puzzle_np=puzzle_csv_df.to_numpy()
-        puzzle_np=puzzle_np.astype("float")
-        puzzle_np[puzzle_np == 0] = np.nan
+        
 
         print(puzzle_np)
-
+        self.puzzle_np =puzzle_np
         return 
 
+    def make_blocks2d(self):
+        puzzle_np=self.puzzle_np
+        chunks=[np.vsplit(sub, 3) for sub in np.hsplit(puzzle_np, 3)]
+
+        return chunks
 
 
 
 theSudoku=Sudoku("s1.csv")
+
+chunks=theSudoku.make_blocks2d()
+for x in range (0, 3):
+    for y in range (0, 3):
+        print(chunks[x][y])
+
+
 
